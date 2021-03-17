e1d8801d32b4b3cee369cb18111fb7ced4dd0397
lcfolkes <lars.folkestad@gmail.com>
2021-03-17 16:03:54 +0100

    moved some helper methods to a separate file helper_funcitons_heuristics.py

---
+++ src/Heuristics/helper_functions_heuristics.py
@@ -0,0 +1,27 @@
+
+def get_first_stage_solution_list_from_dict(first_stage_solution):
+	first_stage_solution_list = []
+	for k, v in first_stage_solution.items():
+		for i in range(len(v)):
+			first_stage_solution_list.append(v[i])
+	return first_stage_solution_list
+
+
+def get_second_stage_solution_dict(input_solution, num_first_stage_tasks):
+	second_stage_solution = {}
+	for k, v in input_solution.items():
+		second_stage_solution[k] = []
+		for s in range(len(input_solution[k])):
+			second_stage_solution[k].append([])
+			for i in range(num_first_stage_tasks, len(input_solution[k][s])):
+				second_stage_solution[k][s].append(input_solution[k][s][i])
+	return second_stage_solution
+
+
+def get_second_stage_solution_list_from_dict(second_stage_solution_dict, num_scenarios):
+	second_stage_solution = [[] for _ in range(num_scenarios)]
+	for k, v in second_stage_solution_dict.items():
+		for s in range(num_scenarios):
+			for i in range(len(v[s])):
+				second_stage_solution[s].append(v[s][i])
+	return second_stage_solution
