58419c912e0a23a7d942deaddcc73aaeb864d4b1
lcfolkes <lars.folkestad@gmail.com>
2021-03-25 15:24:58 +0100

    implemented weight record and updating weights correctly now after each segment

--- src/Heuristics/alns.py
+++ src/Heuristics/alns.py
@@ -13,10 +13,10 @@ os.chdir(path_to_src)
 
 os.chdir(path_to_src)
 
-_IS_BEST = 3
-_IS_BETTER = 2
-_IS_ACCEPTED = 1
-_IS_REJECTED = 0
+_IS_BEST = 3.0
+_IS_BETTER = 2.0
+_IS_ACCEPTED = 1.0
+_IS_REJECTED = 0.0
 
 '''
 
@@ -40,8 +40,11 @@ class ALNS():
         self.filename = filename
         print(filename)
 
-        self.destroy_operators = OrderedDict({'random': 1, 'worst': 1, 'shaw': 1})
-        self.repair_operators = OrderedDict({'greedy': 1, 'regret2': 1, 'regret3': 1})
+        self.destroy_operators = OrderedDict({'random': 1.0, 'worst': 1.0, 'shaw': 1.0})
+        self.repair_operators = OrderedDict({'greedy': 1.0, 'regret2': 1.0, 'regret3': 1.0})
+        self.destroy_operators_record = OrderedDict({'random': [1.0, 0], 'worst': [1.0, 0], 'shaw': [1.0, 0]})
+        self.repair_operators_record = OrderedDict({'greedy': [1.0, 0], 'regret2': [1.0, 0], 'regret3': [1.0, 0]})
+
         self._callbacks = {}
 
         self.best_solution = None
@@ -50,57 +53,60 @@ class ALNS():
 
     def run(self):
         visited_hash_keys = set()
-        it = 20
 
         solution = ConstructionHeuristic(self.filename)
         solution.add_car_moves_to_employees()
         best_solution = copy.deepcopy(solution)
         current_solution = copy.deepcopy(solution)
-        visited_hash_keys.add(best_solution.hash_key)
+        visited_hash_keys.add(current_solution.hash_key)
         # TODO: this is the old objective function val
         best_obj_val = solution.get_obj_val()
         current_obj_val = best_obj_val
         obj_vals = [best_obj_val]
-        while it > 0:
-            if it % 10 == 0:
-                print(f"Iteration {100-it}")
+
+        # SEGMENTS
+        for i in range(10):
+
+            for j in range(10):
+                print(f"Iteration {i*100 + j}")
                 print(f"Best objective value {best_obj_val}")
 
-            solution = copy.deepcopy(current_solution)
-            destroy = self._get_destroy_operator(solution=solution.assigned_car_moves,
-                                           num_first_stage_tasks=solution.world_instance.first_stage_tasks,
-                                           neighborhood_size=2, randomization_degree=1,
-                                           parking_nodes=solution.parking_nodes)
+                solution = copy.deepcopy(current_solution)
+                destroy = self._get_destroy_operator(solution=solution.assigned_car_moves,
+                                               num_first_stage_tasks=solution.world_instance.first_stage_tasks,
+                                               neighborhood_size=2, randomization_degree=1,
+                                               parking_nodes=solution.parking_nodes)
 
-            repair = self._get_repair_operator(destroyed_solution_object=destroy,
-                                         unused_car_moves=solution.unused_car_moves,
-                                         parking_nodes=solution.parking_nodes,
-                                         world_instance=solution.world_instance)
-            solution.rebuild(repair.repaired_solution)
+                repair = self._get_repair_operator(destroyed_solution_object=destroy,
+                                             unused_car_moves=solution.unused_car_moves,
+                                             parking_nodes=solution.parking_nodes,
+                                             world_instance=solution.world_instance)
+                solution.rebuild(repair.repaired_solution)
 
-            if solution.hash_key in visited_hash_keys:
-                #print("Solution already visited")
-                pass
-            else:
-                #update scores for repair and destroy
-                visited_hash_keys.add(solution.hash_key)
-                obj_val = solution.get_obj_val()
-                obj_vals.append(obj_val)
-
-                #Add acceptance criteria
-                if obj_val > current_obj_val:
-                    if obj_val > best_obj_val:
-                        best_solution = copy.deepcopy(solution)
-                        self._update_score_adjustment_parameters(_IS_BEST, destroy, repair)
-                    else:
-                        self._update_score_adjustment_parameters(_IS_BETTER, destroy, repair)
+                if solution.hash_key in visited_hash_keys:
+                    #print("Solution already visited")
+                    pass
                 else:
-                    self._update_score_adjustment_parameters(_IS_ACCEPTED, destroy, repair)
+                    #update scores for repair and destroy
+                    visited_hash_keys.add(solution.hash_key)
+                    obj_val = solution.get_obj_val()
+                    obj_vals.append(obj_val)
 
-                current_solution = copy.deepcopy(solution)
+                    #Add acceptance criteria
+                    if obj_val > current_obj_val:
+                        if obj_val > best_obj_val:
+                            best_solution = copy.deepcopy(solution)
+                            self._update_weight_record(_IS_BEST, destroy, repair)
+                        else:
+                            self._update_weight_record(_IS_BETTER, destroy, repair)
+                    else:
+                        self._update_weight_record(_IS_ACCEPTED, destroy, repair)
+
+                    current_solution = copy.deepcopy(solution)
 
 
-            it -= 1
+            self._update_score_adjustment_parameters()
+
 
         self.best_solution = best_solution
         self.best_obj_val = best_obj_val
@@ -116,6 +122,7 @@ class ALNS():
         w_sum_destroy = sum(w for o, w in self.destroy_operators.items())
         w_dist_destroy = [w / w_sum_destroy for o, w in self.destroy_operators.items()]
         operator = random.choices(list(self.destroy_operators), w_dist_destroy)[0]
+        self.destroy_operators_record[operator][1] += 1
 
         if operator == "random":
             return RandomRemoval(solution, num_first_stage_tasks, neighborhood_size)
@@ -132,6 +139,7 @@ class ALNS():
         w_sum_repair = sum(w for o, w in self.repair_operators.items())
         w_dist_repair = [w / w_sum_repair for o, w in self.repair_operators.items()]
         operator = random.choices(list(self.repair_operators), w_dist_repair)[0]
+        self.repair_operators_record[operator][1] += 1
 
         if operator == "greedy":
             return GreedyInsertion(destroyed_solution_object, unused_car_moves, parking_nodes, world_instance)
@@ -142,36 +150,54 @@ class ALNS():
         else:
             exit("Repair operator does not exist")
 
-    def _update_score_adjustment_parameters(self, operator_score, destroy, repair):
+
+
+    def _update_weight_record(self, operator_score, destroy, repair):
 
         # DESTROY
         if isinstance(destroy, RandomRemoval):
-            self.destroy_operators['random'] += operator_score
+            self.destroy_operators_record['random'][0] += operator_score
         elif isinstance(destroy, WorstRemoval):
-            self.destroy_operators['worst'] += operator_score
+            self.destroy_operators_record['worst'][0] += operator_score
         elif isinstance(destroy, ShawRemoval):
-            self.destroy_operators['shaw'] += operator_score
+            self.destroy_operators_record['shaw'][0] += operator_score
 
         # REPAIR
         if isinstance(repair, GreedyInsertion):
-            self.repair_operators['greedy'] += operator_score
+            self.repair_operators_record['greedy'][0] += operator_score
         elif isinstance(repair, RegretInsertion):
             if repair.regret_nr == 2:
-                self.repair_operators['regret2'] += operator_score
+                self.repair_operators_record['regret2'][0] += operator_score
             elif repair.regret_nr == 3:
-                self.repair_operators['regret3'] += operator_score
+                self.repair_operators_record['regret3'][0] += operator_score
 
+    def _update_score_adjustment_parameters(self):
+        reaction_factor = 0.5
+
+        for k, v in self.destroy_operators.items():
+            self.destroy_operators[k] = self.destroy_operators[k]*(1.0-reaction_factor) + \
+                                        (reaction_factor/self.destroy_operators_record[k][1])*self.destroy_operators_record[k][0]
+            self.destroy_operators_record[k][0] = self.destroy_operators[k]
+            self.destroy_operators_record[k][0] = 0
+
+        for k, v in self.repair_operators.items():
+            self.repair_operators[k] = self.repair_operators[k]*(1.0-reaction_factor) + \
+                                        (reaction_factor/self.repair_operators_record[k][1])*self.repair_operators_record[k][0]
+            self.repair_operators_record[k][0] = self.repair_operators[k]
+            self.repair_operators_record[k][0] = 0
 
 
 if __name__ == "__main__":
     filename = "InstanceGenerator/InstanceFiles/6nodes/6-3-2-1_special_case"
     alns = ALNS(filename + ".pkl")
-    print("Optimal solution")
-    gi = GurobiInstance(filename + ".yaml")
-    run_model(gi)
+
     print("Evaluate solution")
     gi = GurobiInstance(filename + ".yaml", employees=alns.best_solution.employees, optimize=False)
     run_model(gi)
+    #print("Optimal solution")
+    #gi2 = GurobiInstance(filename + ".yaml")
+    #run_model(gi2)
+
 
     # TODO: check objective function
 
