90ba11ba040120a85564d53f2602c6724e06f459
lcfolkes <lars.folkestad@gmail.com>
2021-05-06 17:26:14 +0200

    updated pathmanager

--- src/path_manager.py
+++ src/path_manager.py
@@ -2,6 +2,10 @@ from pathlib import Path
 home = str(Path.home())
 if home == '/Users/mathiasklev':
     src_path = '/Documents/10.semester/masteroppgave/src'
-else:
+elif home == '/Users/larsfolkestad':
     src_path = '/Documents/Skole/NTNU/Master/masteroppgave/src/'
+elif home == '/home/icfolkes':
+    src_path = '/storage/users/icfolkes/masteroppgave/src/'
+elif home == '/home/mathiadk':
+    src_path = '/storage/users/mathiadk/masteroppgave/src/'
 path_to_src = home+src_path
