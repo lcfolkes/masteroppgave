b3a2e97e744ff6da6bc57385cdb27f60df0bb849
lcfolkes <lars.folkestad@gmail.com>
2021-05-13 15:26:04 +0200

    testing imporved

--- src/Testing/parameter_tuning_main.py
+++ src/Testing/parameter_tuning_main.py
@@ -3,6 +3,7 @@ from path_manager import path_to_src
 from Gurobi.Model.gurobi_heuristic_instance import GurobiInstance
 from Gurobi.Model.run_model import run_model
 import os
+import time
 import sys
 os.chdir(path_to_src)
 import multiprocessing as mp
@@ -21,8 +22,28 @@ if __name__ == "__main__":
     filename = "InstanceGenerator/InstanceFiles/30nodes/30-10-2-1_a"
 
     try:
-       obj_vals = parallel_runs(filename, 10)
-       print(obj_vals)
+        n = 10
+
+        ### PARALLEL
+        print(f"Running {n} processes in parallel\n")
+        start = time.perf_counter()
+        obj_vals = parallel_runs(filename, n)
+        finish = time.perf_counter()
+        print(f"Parallel: Finished {n} runs in {round(finish - start, 2)} seconds(s)")
+        print(obj_vals)
+
+        ### SEQUENTIAL
+        print(f"Running {n} processes in sequence\n")
+        start = time.perf_counter()
+        obj_vals = []
+        for _ in range(n):
+            alns = ALNS(filename + ".pkl", acceptance_percentage=1)
+            obj_val = alns.run(False)
+            obj_vals.append(obj_val)
+        finish = time.perf_counter()
+        print(f"Sequential: Finished {n} runs in {round(finish - start, 2)} seconds(s)")
+        print(obj_vals)
+
 
     except KeyboardInterrupt:
         print('Interrupted')
